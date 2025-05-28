ed6343df7a5755da9ac9ba7c426206a2a1c4c6d4
gmirilla <gmirilla@gmail.com>
2025-05-28 07:47:40 +0100

    aa

--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -337,7 +337,7 @@ class PolicyController extends Controller
                 # code...
 
                 $policy->elite_msg=$data['data']['status'] .$data['data']['message'] .$accesstoken;
-                $policy->policyno=$data['data']['policyNo'];
+                $policy->policyno=$data['data']['policy_number'];
                 $policy->status='approved';
                 $policy->save();
                 #Get Agent Credit Balance and change to reflect success;
