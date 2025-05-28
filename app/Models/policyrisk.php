6eba75422859535c2947027bb7e0bd17ba8eb6fb
gmirilla <gmirilla@gmail.com>
2025-05-28 09:47:09 +0100

    1

--- app/Models/policyrisk.php
+++ app/Models/policyrisk.php
@@ -13,13 +13,13 @@ class policyrisk extends Model
 
     public function getvmakeid()
     {
-         $vmakeid= VehicleMake::where('name', $this->vehiclemake)->first();
+         $vmakeid= vehicleMake::where('name', $this->vehiclemake)->first();
          return $vmakeid ? $vmakeid->id : null;
 
     }
     public function getvmodelid()
     {
-         $vmodelid= VehicleModel::where('name', $this->vehiclemodel)->first();
+         $vmodelid= vehicleModel::where('name', $this->vehiclemodel)->first();
          return $vmodelid ? $vmodelid->id : null;
 
     }
