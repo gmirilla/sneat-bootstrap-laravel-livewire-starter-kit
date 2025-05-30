8a5cdee7e6a38ffb1ea581d030dce3ce4f1711a4
gmirilla <gmirilla@gmail.com>
2025-05-30 09:11:50 +0100

    fix: Correct table name casing in users migration

--- database/migrations/2025_05_29_162329_add_new_columns_to_users_table.php
+++ database/migrations/2025_05_29_162329_add_new_columns_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
             $table->integer('stateid')->nullable();
             $table->integer('lgaid')->nullable();
@@ -23,8 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropColumn('stateid');
+            $table->dropColumn('lgaid');
         });
     }
 };
