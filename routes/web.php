428f20013c60689d4f7908a24ae3ec67db0b7c9d
gmirilla <gmirilla@gmail.com>
2025-06-03 16:21:51 +0100

    refactor: Clean up route definitions for better organization and readability

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::middleware('auth')->group(function () {
     Route::post('confirm_mpolicy',[PolicyController::class, 'confirmmpolicy'])->name('confirm_mpolicy');
     Route::post('pay_policy',[PolicyController::class, 'paypolicy'])->name('pay_policy');
     Route::get('test_async',[PolicyController::class, 'testasync'])->name('test_async');
+    Route::get('retry_niip',[PolicyController::class, 'retryniip'])->name('retry_niip');
 
 
 });
