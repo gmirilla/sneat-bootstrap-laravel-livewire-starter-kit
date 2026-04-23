94a2748c87b8b88a8196ef06ff88f470f93972bb
gmirilla <gmirilla@gmail.com>
2026-04-23 11:08:28 +0100

    fix typo

--- resources/views/policy/policylist.blade.php
+++ resources/views/policy/policylist.blade.php
@@ -708,8 +708,7 @@
                 <div class="sl-notice text-danger">
                     <i class="fa-solid fa-triangle-exclamation"></i>
                     Please be informed that sucessful upload to NIIP/NIID with this app is reliant on 
-                     your selection of the appropriate insurance type for your vehicle type<strong>
-                    </strong> as per the options below. Incorrect selection may lead to NIIP/NIID upload failure 
+                     your selection of the appropriate insurance type for your vehicle type as per the options below. Incorrect selection may lead to NIIP/NIID upload failure 
                     and potential issues with your policy. If you are unsure about which insurance type to select, please consult with your insurance provider or agent before proceeding.
                 </div>
 
